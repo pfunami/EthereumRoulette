ff211205ceaffce2134f41cfbe0c27b1ac680d9a
D Nishiyama <nishiyama.daiki.tu@alumni.tsukuba.ac.jp>
2019-06-21 17:56:06 +0900

    error: index.js:2 Uncaught ReferenceError: require is not defined ????????

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,11 +1,24 @@
-import * as res from "express";
+// cryptoをインポート
+const crypto = require('crypto');
 
-var express = require('express');
-var router = express.Router();
-var RanNum = require('./makeRanNum.js');
+// crypto.randomBytes()で生成するときのバイト数
+const nBytes = 4;
+// nBytesの整数の最大値
+const maxValue = 4294967295;
 
-$('#spin').on('click', function () {
-    let ranNum = RanNum.secureRandom().toString();
+// [0.0, 1.0]区間でセキュアな乱数を生成する
+function secureRandom() {
+    // nBytesバイトのランダムなバッファを生成する
+    const randomBytes = crypto.randomBytes(nBytes);
+    // ランダムなバッファを整数値に変換する
+    const r = randomBytes.readUIntBE(0, nBytes);
+    // 最大値で割ることで、[0.0, 1.0]にする
+    return r / maxValue;
+}
+
+document.getElementById('spin').onclick = function () {
+    let randomNum = secureRandom();
+    let ranNum = randomNum.toString();
     console.log(ranNum);
-    res.render('index', {random_num: ranNum});
-});
+    document.getElementById("output").innerHTML = "クリックされた！";
+};
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var RanNum = require('../public/javascripts/makeRanNum.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
